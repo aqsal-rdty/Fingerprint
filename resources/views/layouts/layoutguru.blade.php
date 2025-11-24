74695a711b7915d24bc2d22fd566b74b0b5689c0
Muhammad <muhammadaqsalradityafadilah@smkwikrama.sch.id>
2025-11-24 07:42:35 +0700

    update

--- resources/views/layouts/layoutguru.blade.php
+++ resources/views/layouts/layoutguru.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{ config('app.name') }} - Guru</title>
+  <title>Aplikasi Guru</title>
 </head>
 
 <body>
